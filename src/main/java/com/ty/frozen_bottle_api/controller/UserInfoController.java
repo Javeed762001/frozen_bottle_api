0bdf31df783bfc2bd62896c8db16299987b87a57
Raki0110 <rakeshgowdavviet2000@gmail.com>
2024-01-09 12:20:15 +0530

    staff customer created

--- src/main/java/com/ty/frozen_bottle_api/controller/UserInfoController.java
+++ src/main/java/com/ty/frozen_bottle_api/controller/UserInfoController.java
@@ -3,9 +3,11 @@ package com.ty.frozen_bottle_api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ty.frozen_bottle_api.dto.ResponseStructure;
+import com.ty.frozen_bottle_api.entity.FoodProducts;
 import com.ty.frozen_bottle_api.entity.UserInfo;
 import com.ty.frozen_bottle_api.service.UserInfoService;
 
@@ -19,6 +21,18 @@ public class UserInfoController {
 	public ResponseEntity<ResponseStructure<UserInfo>> saveManager()
 	{
 		return userInfoService.saveManager();
+	}
+	
+	@PostMapping("/savestaff")
+	public ResponseEntity<ResponseStructure<UserInfo>> saveStaff(@RequestBody UserInfo userInfo)
+	{
+		return userInfoService.saveStaff(userInfo);
+	}
+	
+	@PostMapping("/savecustomer")
+	public ResponseEntity<ResponseStructure<UserInfo>> saveCustomer(@RequestBody UserInfo userInfo)
+	{
+		return userInfoService.saveCustomer(userInfo);
 	}
 
 }
